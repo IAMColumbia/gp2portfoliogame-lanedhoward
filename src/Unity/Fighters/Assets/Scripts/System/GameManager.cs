234cdf6e6ff26d96818c19b1652370a8b4ca3de7
Lane Howard <lhoward2@colum.edu>
2023-05-09 01:38:52 -0500

    class diagram

--- src/Unity/Fighters/Assets/Scripts/System/GameManager.cs
+++ src/Unity/Fighters/Assets/Scripts/System/GameManager.cs
@@ -252,7 +252,7 @@ public class GameManager : MonoBehaviour
         yield return new WaitForSeconds(1.5f);
         Announcer.SetActive(false);
 
-        yield return new WaitForSeconds(3f);
+        yield return new WaitForSeconds(2.5f);
 
         if (player1lives <= 0 || player2lives <= 0)
         {
